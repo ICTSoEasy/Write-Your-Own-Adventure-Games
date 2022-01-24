fe526d1391514b25b2b92c0928981527efba9d14
ICT So Easy <info@ictsoeasy.co.uk>
2022-01-24 17:31:53 +0000

    Tidy Up

---
+++ Files/Tidy Up/main.py
@@ -0,0 +1,7 @@
+#import the classes we have written
+from GAME import Game
+from ROOM import Room
+from CREATE import Create
+#Create (instantiate) an instance of Game, called game
+game = Game()
+Create(game)
